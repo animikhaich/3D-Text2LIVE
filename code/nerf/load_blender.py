df81e413b1767772744b9b49af3939b635c299d8
Himanshu Patil <hipatil1998@gmail.com>
2022-11-29 15:12:34 -0500

    fixed bug of taking all images

--- code/nerf/load_blender.py
+++ code/nerf/load_blender.py
@@ -39,7 +39,9 @@ def pose_spherical(theta, phi, radius):
 
 
 def load_blender_data(basedir, half_res=False, testskip=1):
-    splits = ['train', 'val', 'test']
+    # splits = ['train', 'val', 'test']
+    splits = ['train']
+
     metas = {}
     for s in splits:
         with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
@@ -58,8 +60,12 @@ def load_blender_data(basedir, half_res=False, testskip=1):
             skip = testskip
             
         for frame in meta['frames'][::skip]:
-            fname = os.path.join(basedir, frame['file_path'] + '.png')
-            imgs.append(imageio.imread(fname))
+            try:
+                fname = os.path.join(basedir, frame['file_path'] + '.png')
+                imgs.append(imageio.imread(fname))
+            except:
+                continue
+            
             poses.append(np.array(frame['transform_matrix']))
         imgs = (np.array(imgs) / 255.).astype(np.float32) # keep all 4 channels (RGBA)
         poses = np.array(poses).astype(np.float32)
