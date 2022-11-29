cf62367cf3728ca483d1871de8faa2d9758c0996
Animikh Aich <animikhaich@gmail.com>
2022-11-29 14:05:42 +0530

    Added: Multi Image Text2LIVE for NeRF

--- code/stable-dreamfusion/run.sh
+++ code/stable-dreamfusion/run.sh
@@ -1,3 +1,3 @@
-python3 main.py -O --text 'a standing stanford bunny' --workspace stanford_bunny --iters 50000 --lr 1e-3 --w 32 --h 32 --seed 0 --lambda_entropy 1e-5 --ckpt scratch --save_mesh --albedo_iters 10000
+python3 main.py -O --text 'a DSLR photo of a squirrel' --workspace squirrel --iters 100000 --lr 1e-3 --w 64 --h 64 --seed 333 --lambda_entropy 1e-5 --ckpt scratch --save_mesh --albedo_iters 10000
 
-python3 main.py -O --test --workspace stanford_bunny --save_mesh
+python3 main.py -O --test --workspace squirrel --save_mesh
